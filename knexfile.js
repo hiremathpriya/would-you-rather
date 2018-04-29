ffc024f33810dfdeb2a446ab496a3e156a87c7f7
Priya Hiremath <priyahrish18@gmail.com>
2018-04-29 20:03:11 +1200

    heroku db connection changes

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,9 @@
 
 // Update with your config settings.
 
-module.exports = {
+const DB_URL = 'postgres://uwpfaiydowndrn:7756c6d653b1d4487f450b1044066d13733c23f9603c27a1a759b43b547e5588@ec2-107-21-126-193.compute-1.amazonaws.com:5432/d6eotg573vain?ssl=true';
 
-  
+module.exports = {
 
   development: {
     client: 'sqlite3',
@@ -11,38 +11,29 @@ module.exports = {
       filename: './dev.sqlite3'
     },
     useNullAsDefault: true
-},
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
-
+  
   production: {
-    client: 'postgresql',
+    client: 'pg',
+    /*
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+      host : 'ec2-107-21-126-193.compute-1.amazonaws.com',
+      database: 'd6eotg573vain',
+      user:     'uwpfaiydowndrn',
+      password: '7756c6d653b1d4487f450b1044066d13733c23f9603c27a1a759b43b547e5588'
+
+    },*/
+    connection: DB_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
-    }
+    },
+    debug: true,
+    ssl: true
   }
+
 
 };
